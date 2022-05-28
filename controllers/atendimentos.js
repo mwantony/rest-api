8e1fe4237eb79eac4773dec502a5016cf24b94a8
Antony Maziero Warken <againplayi7@gmail.com>
2022-05-28 15:02:01 -0300

    rest api cap 2 vid 2 finalizado

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,9 @@
 module.exports = (app) => {
-  app.get("/atendimento", (req, res) =>
+  app.get("/atendimentos", (req, res) =>
     res.send("Você está na rota de atendimento e está realizando um GET")
   );
+  app.post('/atendimentos', (req, res) => {
+    console.log(req.body)
+    res.send('Você está na rota de atendimentos e está realizando um POST')
+  })
 };
