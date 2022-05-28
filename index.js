f6c5fffd0b26ade98c457b86496a479b6db65e01
Antony Maziero Warken <againplayi7@gmail.com>
2022-05-28 15:41:06 -0300

    rest api cap 3 vid 1 finalizado

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const customExpress = require('./config/customExpress.js')
-const app = customExpress()
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+const customExpress = require("./config/customExpress.js");
+const conexao = require("./infraestrutura/conexao.js");
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log("Conectado com sucesso");
+    const app = customExpress();
+    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+  }
+});
